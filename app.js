d338f9db0c2e624b6a0c0b2468e19cc5cf81a2dc
Adam Pasternack <adam.pasternack@gmail.com>
2016-11-08 08:35:06 -0500

    updates

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 
 var greet = require('./greet');
 greet();
+
--- constructor.js
+++ constructor.js
@@ -11,8 +11,15 @@ Person.prototype.greet = function() {
 
 Person.prototype.middlename = 'Todd';
 
+
+// when new is used before a function, it redirects this to a new empty object which then has properties set as the function (here, Person) is run:
 var john = new Person('John', 'Doe');
+
+var jane = new Person('Jane', 'Doe');
+jane.greet();
 
 john.greet();
 
 console.log(john.__proto__);
+console.log(jane.__proto__);
+console.log(john.__proto__ === jane.__proto__);
