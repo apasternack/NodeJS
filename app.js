3491364ee6c1b0fe5546c637489fa23b1f5e34c3
Adam Pasternack <adam.pasternack@gmail.com>
2016-11-09 16:17:07 -0500

    module pattern 4

--- app.js
+++ app.js
@@ -8,4 +8,13 @@ greet2();
 
 var greet3 = require('./greet3');
 greet3.greet();
+greet3.greeting = 'changed hello world!';
 
+//require CASHES THINGS so whenever it is called subsequently EVEN FROM A DIFFERENT FILE, a reference to the origional object set to module.exports is returned 
+var greet3b = require('./greet3');
+greet3b.greet();
+
+// returning a Constructor function instead of a new object made by the constructor function is a way around the above limitation 
+var Greet4 = require('./greet4');
+var grtr = new Greet4();
+grtr.greet();
