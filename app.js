ecb8ec28051b33d77a9939cb0dbcff7003529169
Adam Pasternack <adam.pasternack@gmail.com>
2016-11-11 07:57:34 -0500

    exports vs module.exports

--- app.js
+++ app.js
@@ -1,23 +1,5 @@
-var greet = require('./greet1');
-greet();
+var greet = require('./greet0');
+var greet2 = require('./greet0b');
 
-// var greet2 = require('./greet2');
-// greet2.greet();
-var greet2 = require('./greet2').greet;
-greet2();
+greet2.greet();
 
-var greet3 = require('./greet3');
-greet3.greet();
-greet3.greeting = 'changed hello world!';
-
-//require CASHES THINGS so whenever it is called subsequently EVEN FROM A DIFFERENT FILE, a reference to the origional object set to module.exports is returned 
-var greet3b = require('./greet3');
-greet3b.greet();
-
-// returning a Constructor function instead of a new object made by the constructor function is a way around the above limitation 
-var Greet4 = require('./greet4');
-var grtr = new Greet4();
-grtr.greet();
-
-var greet5 = require('./greet5').greet;
-greet5();
